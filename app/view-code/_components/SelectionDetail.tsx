1c33bcc741c31592a4326896d683f914b4aed7ec
Aneerudh17 <paneerudh@gmail.com>
2026-04-14 23:02:16 +0530

    major ui update

--- app/view-code/_components/SelectionDetail.tsx
+++ app/view-code/_components/SelectionDetail.tsx
@@ -8,18 +8,18 @@ import { RefreshCcw } from 'lucide-react'
 
 function SelectionDetail({ record, regenrateCode, isReady }: any) {
     return record && (
-        <div className='p-5 bg-gray-100  rounded-lg'>
+        <div className='p-5 bg-black rounded-lg'>
             <h2 className='font-bold my-2'>Wireframe</h2>
             <Image src={record?.imageUrl} alt='Wireframe' width={300} height={400}
-                className='rounded-lg object-contain h-[200px] w-full border  border-dashed p-2 bg-white'
+                className='rounded-lg object-contain h-[200px] w-full border  border-dashed p-2 bg-grey-200'
             />
 
-            <h2 className='font-bold mt-4 mb-2'>AI Model</h2>
-            <Input defaultValue={record?.model} disabled={true} className='bg-white' />
+            <h2 className='font-bold mt-4 mb-2 '>AI Model</h2>
+            <Input defaultValue={record?.model} disabled={false} className='bg-grey-200' />
 
             <h2 className='font-bold mt-4 mb-2'>Description</h2>
-            <Textarea defaultValue={record?.description} disabled={true}
-                className='bg-white h-[180px]' />
+            <Textarea defaultValue={record?.description} disabled={false}
+                className='bg-black h-[180px]' />
 
             <Button className='mt-7 w-full' disabled={!isReady} onClick={() => regenrateCode()} > <RefreshCcw /> Regenerate Code</Button>
         </div>
